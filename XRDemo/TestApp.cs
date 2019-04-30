d4aaa675e763c7f98ed509f63899ee8387dcd019
Kiran Pande <kiranpande1987@gmail.com>
2019-05-01 04:32:26 +0530

    Automation Project added. Initial Checkins

---
+++ XRDemo/TestApp.cs
@@ -0,0 +1,22 @@
+﻿using System;
+namespace XRDemo
+{
+    public class TestApp
+    {
+        public TestApp()
+        {
+        }
+
+        [Test]
+        public void MyFirstTest()
+        {
+            var app = ConfigureApp
+              .Android
+              .ApkFile(“MyApkFile.apk”)
+              .StartApp();
+
+            app.EnterText(“NameField”, “Rasmus”);
+            app.Tap(“SubmitButton”);
+        }
+    }
+}
---
+++ XRDemoTest/SampleTest.cs
@@ -0,0 +1,22 @@
+﻿using NUnit.Framework;
+using System;
+using Xamarin.UITest;
+
+namespace XRDemoTest
+{
+    [TestFixture()]
+    public class SampleTest
+    {
+        [Test()]
+        public void TestCase()
+        {
+            var app = ConfigureApp
+                            .Android
+                            .ApkFile("/Users/kiran/Desktop/xrdemoapk/app.apk")
+                            .StartApp();
+
+            app.Tap("Basic UI Demo");
+            app.Tap("SUBMIT");
+        }
+    }
+}
